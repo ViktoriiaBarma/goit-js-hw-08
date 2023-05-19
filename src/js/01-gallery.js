b556aa9517dbafe66d9a8c5b742b67a8713e89c3
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-05-19 13:20:29 +0300

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+
+function createGalleryItemsMarkup(elem) {
+  return elem
+    .map(({ preview, original, description }) => {
+      return ` <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}" 
+        />
+   </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 200,
+});
